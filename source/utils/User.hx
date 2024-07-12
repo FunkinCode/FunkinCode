f1c0d48c5aabf6faa806a6b4eda951cc3436a3b2
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-12 16:36:30 -0600

    hi :3 | Update source/utils/User.hx

---
+++ source/utils/User.hx
@@ -0,0 +1,12 @@
+package utils;
+
+typedef User = {
+    var ID: String;
+    var Username: String;
+    var DisplayUsername: String ;
+    var AvatarURL: String ;
+    var Flags: Int;
+    var CreatedAt: Date;
+    var DiscordID:String;
+
+}
