ce7445e37b9ae7973ebc7a07b2a942c7de2f10d2
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-12 16:36:16 -0600

    hi :3 | Update source/FPS.hx

--- source/FPS.hx
+++ source/FPS.hx
@@ -3,24 +3,36 @@ import openfl.system.System;
 import openfl.display.FPS as OpenFLFPS;
 class FPS extends OpenFLFPS {
     var oldText:String = "";
+    public static var anotherSumText:String = "";
+    public static var maxMemPeaked:Float = 0;
+    public static var memoryUsed:Float;
     override function __enterFrame(deltaTime:Float):Void {
         oldText = text;
+      var fps = PreferencesMenu.getPref("fps");
+		eval('FlxG.elapsed = ${1 / 24};');
         super.__enterFrame(deltaTime);
+        if (currentFPS >=   fps ) {
+            text = 'FPS: ${fps}' ;
+        }
         if (oldText != text){
-        var memoryMegas:Float = 0;
-			
-        #if openfl
-        memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
-        text += "\nMemory: " + memoryMegas + " MB";
-        #end
+        width = 350;
+        var memoryMegas:Float = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
+
+        memoryUsed = memoryMegas;
+
+        text += "\nMemory: " + memoryMegas + ' MB\nMaxMem Peak: ${maxMemPeaked} mb';
+        if (maxMemPeaked < memoryMegas )
+            maxMemPeaked = memoryMegas;     
+    
+
         textColor = 0xFFFFFFFF;
-        var fps = PreferencesMenu.getPref("fps");
+  
         if (Math.isNaN(fps) )
             fps = 60;
         if (memoryMegas > 3000 || currentFPS <= fps / 2)
-        {
-            textColor = 0xFFFF0000;
-        } 
+            {
+                textColor = 0xFFFF0000;
+            } 
         }
 }
 }
