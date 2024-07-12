95dbb1c0f52a0429137edf349c1ca263519a5652
xXMomazosNiz_69Xx <jalipa7u7@gmail.com>
2024-07-12 16:36:19 -0600

    hi :3 | Update source/utils/Section.hx

--- source/utils/Section.hx
+++ source/utils/Section.hx
@@ -11,21 +11,9 @@ typedef SwagSection =
 	var altAnim:Bool;
 }
 
-class Section
-{
-	public var sectionNotes:Array<Dynamic> = [];
-
-	public var lengthInSteps:Int = 16;
-	public var typeOfSection:Int = 0;
-	public var mustHitSection:Bool = true;
-
-	/**
-	 *	Copies the first section into the second section!
-	 */
-	public static var COPYCAT:Int = 0;
-
-	public function new(lengthInSteps:Int = 16)
-	{
-		this.lengthInSteps = lengthInSteps;
-	}
+typedef Section = {
+	var notes:Array<Array<Dynamic>>;
+	var bpm:Float;
+	var mustHitSection:Bool;
+	var changeBPM:Bool;
 }
